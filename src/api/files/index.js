c41ff65dc84201edb16ad093bab6671c9c160540
annoinspace <aneesahalmask@gmail.com>
2022-12-26 16:42:11 +0000

    image upload works

--- src/api/files/index.js
+++ src/api/files/index.js
@@ -1,8 +1,7 @@
-// router for uploading files/images
 import express from "express"
 import multer from "multer"
 import { extname } from "path"
-import { getProducts, writeProducts, saveProductImage, saveCoverPhoto } from "../../lib/fs-tools.js"
+import { writeProducts, getProducts, saveCoverPhoto } from "../../lib/fs-tools.js"
 
 const filesRouter = express.Router()
 
@@ -10,48 +9,25 @@ filesRouter.post("/:productId/upload", multer().single("imageUrl"), async (req, 
   try {
     const originalFileExtension = extname(req.file.originalname)
     const fileName = req.params.productId + originalFileExtension
-    await saveProductImage(fileName, req.file.buffer)
-    const url = `http://localhost:3001/img/productImages/${fileName}`
+    await saveCoverPhoto(fileName, req.file.buffer)
+    //   url to use for the mew image
+    const url = `http://localhost:3001/img/${fileName}`
+    console.log("url", url)
+
     const products = await getProducts()
     const index = products.findIndex((product) => product._id === req.params.productId)
+    //   updating the blog cover
     if (index !== -1) {
-      const oldProductInfo = products[index]
-      const updatedProductInfo = { ...oldProductInfo, imageUrl: url, updatedAt: new Date() }
-      products[index] = updatedProductInfo
+      const oldProduct = products[index]
+      const coverUpdate = { ...oldProduct, imageUrl: url }
+      const updatedProduct = { ...oldProduct, coverUpdate, updatedAt: new Date() }
+      products[index] = updatedProduct
       await writeProducts(products)
     }
-    res.send("Product image updated")
+    res.send("product image updated")
   } catch (error) {
-    res.send(error)
+    next(error)
   }
 })
-
-// filesRouter.post(
-//     "/:blogId/uploadCover",
-//     multer().single("cover"),
-//     async (req, res, next) => {
-//       try {
-//         const originalFileExtension = extname(req.file.originalname)
-//         const fileName = req.params.blogId + originalFileExtension
-//         await saveCoverPhoto(fileName, req.file.buffer)
-//         //   url to use for the mew image
-//         const url = `http://localhost:3001/img/users/${fileName}`
-
-//         const blogs = await getBlogs()
-//         const index = blogs.findIndex((blog) => blog._id === req.params.blogId)
-//         //   updating the blog cover
-//         if (index !== -1) {
-//           const oldBlog = blogs[index]
-//           // const coverUpdate = { ...oldBlog, cover: url }
-//           const updatedBlog = { ...oldBlog, cover: url, updatedAt: new Date() }
-//           blogs[index] = updatedBlog
-//           await writeBlog(blogs)
-//         }
-//         res.send("Cover image updated")
-//       } catch (error) {
-//         next(error)
-//       }
-//     }
-//   )
 
 export default filesRouter
--- src/lib/fs-tools.js
+++ src/lib/fs-tools.js
@@ -1,22 +1,24 @@
 import { fileURLToPath } from "url"
 import { dirname, join } from "path"
 import fs from "fs-extra"
-import { writeFile } from "fs"
-const { readJSON, writeJSON } = fs
+
+const { readJSON, writeJSON, writeFile } = fs
 
 // for the products
 const dataFolderPath = join(dirname(fileURLToPath(import.meta.url)), "../data")
 const productsJSONPath = join(dataFolderPath, "products.json")
 const reviewsJSONPath = join(dataFolderPath, "reviews.json")
 
+const publicFolderPath = join(process.cwd(), "public/img")
+console.log("Public folder path in fs-tools-----------", publicFolderPath)
+
 export const getProducts = () => readJSON(productsJSONPath)
 export const getReviews = () => readJSON(reviewsJSONPath)
 export const writeProducts = (productsArray) => writeJSON(productsJSONPath, productsArray)
 export const writeReviews = (reviewsArray) => writeJSON(reviewsJSONPath, reviewsArray)
+// export const saveProductImage = (fileName, contentAsABuffer) => {
+//   writeFile(join(publicFolderPath, fileName), contentAsABuffer)
+// }
 
-const publicFolderPath = join(process.cwd(), " ./public/img/productImages")
-export const saveProductImage = (fileName, contentAsABuffer) => {
-  writeFile(join(publicFolderPath, fileName), contentAsABuffer)
-}
 export const saveCoverPhoto = (fileName, contentAsABuffer) =>
   writeFile(join(publicFolderPath, fileName), contentAsABuffer)
